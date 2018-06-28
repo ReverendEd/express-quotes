9bcc316ea01a5e870e782ed8c53aced027313d03
Tyler Sehr <tysehr37@gmail.com>
2018-06-28 16:02:23 -0500

    added post and random quote as well as imgs

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ console.log('server.js is working');
 const express = require('express');
 const app = express();
 const port = 5000;
-
+const bodyParser = require('body-parser');
 
 app.use(express.static('server/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.listen(port, function(){
     console.log('listening on '+port);
@@ -12,13 +16,16 @@ app.listen(port, function(){
 
 let quotes = [{
     quote: 'make it so',
-    author: 'Picard'
+    author: 'Picard',
+    picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/20/Captain_Picard_Chair.jpg/220px-Captain_Picard_Chair.jpg?size=50'
   },{
-    quote: 'want a peanut',
-    author: 'vizzini'
+    quote: 'inconceivable!',
+    author: 'vizzini',
+    picture: 'https://i.pinimg.com/736x/5c/c2/cf/5cc2cff2638ec5a6d9c2098630edcfb8--the-princess-bride-so-funny.jpg?size=50'
   },{
     quote: 'life is like a box of chocolates',
-    author: 'forest gump'
+    author: 'forest gump',
+    picture: 'http://static4.uk.businessinsider.com/image/583ed72ddd0895af198b47d0-480/forrest-gump-tom-hanks-1.jpg?size=50'
   }
   ];
 
@@ -29,22 +36,14 @@ app.get('/quotes', function(req, res){
 app.get('/random', function(req, res){
     res.send(randomQuotes());
 });
-// app.post('/submitquotes'),{
-//     quote: 
-// }
+
+app.post('/submitquotes', function(req, res){
+    quotes.push(req.body);
+    res.sendStatus(201);
+});
 
 function randomQuotes(){
-    let randomNum = Math.floor(Math.random()*3)+1;
+    let randomNum = Math.floor(Math.random()*quotes.length);
+    console.log(randomNum);
     return quotes[randomNum];
 }
-
-
-
-
-
-
-
-
-
-
-module.exports = quotes;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,19 +1,28 @@
 console.log('client.js is working');
 
+class Quote{
+    constructor(quote, author, picture){
+        this.quote = quote;
+        this.author = author;
+        this.picture = picture;
+    }
+}
+
 $(document).ready(pageReady);
 
 function pageReady() {
     console.log('jq is working');
     clickHandler();
-    
 }
 
 function clickHandler(){
     $('#quotesbutton').on('click', retrieveQuotes);
-    $('#submitquote').on('click', submitQuote);
+    $('#buttonIDsubmitquote').on('click', submitQuote);
+    $('#randomquote').on('click', randomQuote);
 }
 
 function retrieveQuotes(){
+    $('#forquotes').empty();
     console.log('in retrieveQuotes');
     $.ajax({
         url: '/quotes',
@@ -30,21 +39,57 @@ function retrieveQuotes(){
 function loopThruQuotes(quoteList){
     console.log('in loopThruQuotes');
     for (let i = 0; i < quoteList.length; i++) {
-        $('#forquotes').append('<li class="table">'+quoteList[i].quote+': "'+quoteList[i].author+'"</li>');
+        $row = $('<tr></tr>');
+        $row.append('<th>'+quoteList[i].quote+'</th>');
+        $row.append('<th>'+quoteList[i].author+'</th>');
+        $row.append('<th><img src="'+quoteList[i].picture+'" alt=""></th>')
+        $('#forquotes').append($row);
     }
 }
 
 function submitQuote(){
     console.log('in submitQuotes');
-    $.post({
-        url: '/submitquotes',
-        type: 'POST'
+    if ($('#inputIDquote').val()!='' && $('#inputIDauthor').val()!='') {
+        let submittedQuote = new Quote($('#inputIDquote').val(), $('#inputIDauthor').val(), $('#inputIDpicture').val());
+        $.ajax({
+            url: '/submitquotes',
+            type: 'POST',
+            data: submittedQuote
+        }).done(function(response){
+            console.log(response);
+            retrieveQuotes(response);
+        }).fail(function(errorMessage){
+            console.log('in errorMessage');
+            alert('your quotes could not be submitted! isnt that an awful feeling? .'+ errorMessage.status);
+        })
+        clearInputs();
+    }
+    else{
+        alert('you havent filled out all the inputs. what did you expect would happen?');
+    }
+}
+
+function randomQuote(){
+    $('#forquotes').empty();
+    console.log('in randomQuote');
+    let $row = $('<tr></tr>')
+    $.ajax({
+        url: '/random',
+        type: 'GET'
     }).done(function(response){
         console.log(response);
+        $row.append('<th>'+response.quote+'</th>');
+        $row.append('<th>'+response.author+'</th>');
+        $row.append('<th><img src="'+response.picture+'" alt=""></th>')
+        $('#forquotes').append($row);
     }).fail(function(errorMessage){
         console.log('in errorMessage');
-        alert('your quotes could not be submitted! isnt that an awful feeling? .'+ errorMessage.status);
+        alert('your quote could not be generated! Im sure something will happen eventually if u just wait...'+errorMessage.status);
     })
 }
 
-
+function clearInputs(){
+    $('#inputIDquote').val('');
+    $('#inputIDauthor').val('');
+    $('#inputIDpicture').val('');
+}
